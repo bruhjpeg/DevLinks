67e9263574220ede80fe12aa73a6f268332b7f92
BrunaAquino22 <brunahasting99@gmail.com>
2023-01-29 22:53:01 -0300

    javascript

---
+++ script.js
@@ -0,0 +1,17 @@
+function toggleMode() {
+  const html = document.documentElement
+
+ // if(html.classList.contains('light')) {
+ //   html.classList.remove('light')
+ // } else {
+ //   html.classList.add('light')
+ // }
+//  }
+//  ou
+
+html.classList.toggle('light') 
+}
+
+function emBreve() {
+  alert('Em breve ⏳')
+}
